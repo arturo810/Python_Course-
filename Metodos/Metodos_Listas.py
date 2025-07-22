602787460210de0355caee99d964577836e3e62b
Arthur <arthur810@icloud.com>
2025-07-22 11:10:23 -0600

    Metodos tuplas, diccionario, listas

--- Metodos/Metodos_Listas.py
+++ Metodos/Metodos_Listas.py
@@ -19,3 +19,24 @@ fruta_eliminada = frutas.pop(2)
 print(frutas)  # Imprime ["manzana", "uva", "pera"]
 print(fruta_eliminada)  # Imprime "naranja"
 
+# Tuplas
+punto = (3, 4)
+print(punto[0])  # Imprime 3
+print(punto[1])  # Imprime 4
+
+# Diccionario
+
+persona = {"nombre": "Juan", "edad": 25, "ciudad": "Madrid"}
+print(persona["nombre"])  # Imprime "Juan"
+print(persona["edad"])    # Imprime 25
+print(persona["ciudad"])  # Imprime "Madrid"
+
+
+# Metodos
+persona = {"nombre": "Juan", "edad": 25, "ciudad": "Madrid"}
+
+print(persona.keys())    # Imprime dict_keys(["nombre", "edad", "ciudad"])
+print(persona.values())  # Imprime dict_values(["Juan", 25, "Madrid"])
+print(persona.items())   # Imprime dict_items([("nombre", "Juan"), ("edad", 25), ("ciudad", "Madrid")])
+persona.update({"profesion": "Ingeniero"})
+print(persona)  # Imprime {"nombre": "Juan", "edad": 25, "ciudad": "Madrid", "profesion": "Ingeniero"}
